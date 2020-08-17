e4a2aecc623d258752a538a701e9007a85a565eb
chocopon <chocopon@192.168.10.101>
2020-08-17 23:00:43 +0900

    delete login functions

--- WebContent/js/app.js
+++ WebContent/js/app.js
@@ -3,12 +3,7 @@ var ViewApp = angular.module('ViewApp', ['ngRoute']);
 ViewApp.config(['$routeProvider', function($routeProvider){
     $routeProvider
     .when('/', {
-      templateUrl: 'templates/menu.html',
-      controller: 'MenuController'
-    })
-    .when('/menu', {
-      templateUrl: 'templates/menu.html',
-      controller: 'MenuController'
+      templateUrl: 'templates/menu.html'
     })
     .when('/connect', {
       templateUrl: 'templates/connect.html',
@@ -35,112 +30,34 @@ ViewApp.config(['$routeProvider', function($routeProvider){
 }]);
 
 
+var url_apis = 'api/resources/apis';
+var url_token = 'api/resources/token';	
 
-ViewApp.controller('LoginController', ['$scope', '$http', '$window','$httpParamSerializerJQLike',
-	 function($scope, $http, $window, $httpParamSerializerJQLike){
-	   $scope.username = null;
-	   $scope.mdusername = null;
-	   $scope.mdemail = null;
-	   
-	   sessionStorage.removeItem('jwt');
-	
-       $scope.submit = function(){
-    	  var method = "POST";	
-    	  var url = 'api/resources/login';	
-    		
-    	  $http({
-    	          method: method,
-    	          headers : {
-                      'Content-Type' : 'application/x-www-form-urlencoded;charset=utf-8'
-                  },
-                  transformRequest: $httpParamSerializerJQLike,
-    	          url: url,
-    	          data: { username: $scope.username, password: $scope.password }
-    	        }).then(function successCallback(response){
-    	        	var resdata = response.data;
-    	        	var jwt = resdata.JWT;
-    	        	sessionStorage.setItem('jwt', jwt);
-    	        	
-    	        	$window.location.href = 'main.html';
-    	        }, function errorCallback(response) {
-    	        	var sts = response.status;
+var clientid = 'a643943f-fd85-4801-9bd4-6c79d3e1d3c2';
+var token = localStorage.getItem('access_token');
 
-    	        	if(sts == 400){
-      	        	   $scope.login_message = 'Password is wrong.';
-    	        	}else{
-         	           $scope.login_message = 'Your ID does not exist.';
-    	        	}
-    	      });
-    	};
-    	
-    	$scope.register = function(){
-      	  var method = "POST";	
-      	  var url = 'api/resources/register';	
-      		
-      	  $http({
-      	          method: method,
-      	          headers : {
-                        'Content-Type' : 'application/x-www-form-urlencoded;charset=utf-8'
-                    },
-                    transformRequest: $httpParamSerializerJQLike,
-      	          url: url,
-      	          data: { username: $scope.mdusername, password: $scope.mdpassword }
-      	        }).then(function successCallback(response){
-      	        	$scope.register_message = 'Registerd your new ID. Please login.';
-      	        }, function errorCallback(response) {
-      	        	$scope.register_message = 'Error occurred. Please check your input.';
-      	        });
-      	};
-
-    }]);
-
-
-ViewApp.controller('MenuController', ['$http', '$location','$httpParamSerializerJQLike',
-	function( $http, $location, $httpParamSerializerJQLike){
-	
-	var method = "POST";	
-	var url = 'api/resources/check';	
-	var jwt = sessionStorage.getItem('jwt');
-
-	
-	$http({
-          method: method,
-          headers : {
-              'Content-Type' : 'application/x-www-form-urlencoded;charset=utf-8'
-          },
-          transformRequest: $httpParamSerializerJQLike,
-          url: url,
-          data: { jwt: jwt}
-        }).then(function successCallback(response){
-	    }, function errorCallback(response) {
-   	        $location.path('/error');
-     })  
-    
-   }]);
-
+var targetpath_body = '/chocopon0899gmailcom-dev/sb/openapi/bodyinformation';
+var targetpath_blood = '/chocopon0899gmailcom-dev/sb/openapi/bloodinformation';
 
 
 ViewApp.controller('ConnectController', ['$http', '$location','$httpParamSerializerJQLike',
 	function( $http, $location, $httpParamSerializerJQLike){
-	var clientid = 'a643943f-fd85-4801-9bd4-6c79d3e1d3c2';
-	var authcode =$location.search()["code"];
-	
-	var method = "POST";	
-	var url = 'api/resources/token';	
-	var jwt = sessionStorage.getItem('jwt');
 
-	
+	var authcode =$location.search()["code"];
+
 	$http({
-          method: method,
+          method: 'POST',
           headers : {
               'Content-Type' : 'application/x-www-form-urlencoded;charset=utf-8'
           },
           transformRequest: $httpParamSerializerJQLike,
-          url: url,
-          data: { clientid: clientid, authcode: authcode, jwt: jwt}
-        }).then(function successCallback(response){
+          url: url_token,
+          data: { clientid: clientid, authcode: authcode}
+
+	   }).then(function successCallback(response){
         	var resdata = response.data;
         	var token = resdata.access_token;
+
         	localStorage.setItem('access_token', token);
 	      
      })  
@@ -152,26 +69,16 @@ ViewApp.controller('ConnectController', ['$http', '$location','$httpParamSeriali
 ViewApp.controller('LineController', ['$scope', '$http', '$location', '$httpParamSerializerJQLike', 
 	function($scope, $http, $location, $httpParamSerializerJQLike){
 
-	  var method = "POST";	
-	  var url = 'api/resources/resourse';	
-
-	  var clientid = 'a643943f-fd85-4801-9bd4-6c79d3e1d3c2';
-	  var token = localStorage.getItem('access_token');
-	  var jwt = sessionStorage.getItem('jwt');
 	  var targetpath = '/chocopon0899gmailcom-dev/sb/openapi/bodyinformation';
 
-	  getinfo1(clientid, token);
-
-	  function getinfo1(clientid, token){
-		  
-			 $http({
-		       method: method,
+	    $http({
+		       method: 'POST',
 		       headers : {
 		           'Content-Type' : 'application/x-www-form-urlencoded;charset=utf-8'
 		       },
 		       transformRequest: $httpParamSerializerJQLike,
-		       url: url,
-		       data: { clientid: clientid, access_token: token,  jwt: jwt,  targetpath: targetpath}
+		       url: url_apis,
+		       data: { clientid: clientid, access_token: token,  targetpath: targetpath_body}
 		     }).then(function successCallback(response){
 
 		          resdata = response.data;
@@ -241,89 +148,23 @@ ViewApp.controller('LineController', ['$scope', '$http', '$location', '$httpPara
 			      
    	        }, function errorCallback(response) {
    	        $location.path('/error');
-		  })
-		};
-
-     function getinfo2(clientid, token){
-			  
-		$http({
-		       method: method,
-		       headers : {
-		           'Content-Type' : 'application/x-www-form-urlencoded;charset=utf-8'
-		       },
-		       transformRequest: $httpParamSerializerJQLike,
-		       url: url,
-		       data: { clientid: clientid, access_token: token, jwt: jwt , targetpath: targetpath}
-		     }).then(function successCallback(response){
-
-		         resdata = response.data;
-
-		         var tmp = dataArray(resdata);
-			     var Labels = createLabels(tmp);
-			     var Data2 = createData2(tmp);
-
-			     $scope.labels2 = Labels;
-			     $scope.data2 = [Data2];
-			     $scope.datasetOverride2 = [{
-			    	  label: "Weight",
-			          borderWidth: 3,
-			          backgroundColor: 'rgba(60, 190, 20, 0.3)',
-			          borderColor: 'rgba(60, 190, 20, 0.8)'
-		        }];
-
-			     $scope.options2 = {
-			    	      scales: {
-			    	        xAxes: [
-			    	          {
-			    	           ticks: {
-			    	              autoSkip: true,
-			    	              maxTicksLimit: 10
-			    	               }
-			    	          }
-			    	        ]
-			    	  }
-			     };
-
-			     var date = new Date();
-			     $scope.time2 = date.toLocaleString('en-GB');
-
-		     }, function errorCallback(response) {
-	   	   	        $location.path('/error');
-			      
-		      })
-		 };
+		  });
 	  	  
-}]);
+ }]);
 
 ViewApp.controller('BarController', ['$scope', '$http', '$location', '$httpParamSerializerJQLike', 
 	function($scope, $http, $location, $httpParamSerializerJQLike){
 
-	  var method = "POST";	
-	  var url = 'api/resources/resourse';	
-
-	  var clientid = 'a643943f-fd85-4801-9bd4-6c79d3e1d3c2';
-	  var token = localStorage.getItem('access_token');
-	  var jwt = sessionStorage.getItem('jwt');
-	  var targetpath = '/chocopon0899gmailcom-dev/sb/openapi/bodyinformation';
-
-	  getinfo3(clientid, token);
-	  
-	  $scope.renew3 = function(){
-		  getinfo3(clientid, token);
-	  };
-
-
-	function getinfo3(clientid, token){
-			  
-				$http({
-			       method: method,
+		  $http({
+			       method: 'POST',
 			       headers : {
 			           'Content-Type' : 'application/x-www-form-urlencoded;charset=utf-8'
 			       },
 			       transformRequest: $httpParamSerializerJQLike,
-			       url: url,
-			       data: { clientid: clientid, access_token: token, jwt: jwt, targetpath: targetpath}
-			     }).then(function successCallback(response){
+			       url: url_apis,
+			       data: { clientid: clientid, access_token: token, targetpath: targetpath_body}
+
+		        }).then(function successCallback(response){
 
 			        resdata = response.data;
 
@@ -365,35 +206,23 @@ ViewApp.controller('BarController', ['$scope', '$http', '$location', '$httpParam
 		   	   	     $location.path('/error');
 				      
 			  })
-	  };
 	  	  
 }]);
 
 ViewApp.controller('RadarController', ['$scope', '$http', '$location', '$httpParamSerializerJQLike', 
 	function($scope, $http, $location, $httpParamSerializerJQLike){
 
-	  var method = "POST";	
-	  var url = 'api/resources/resourse';	
 
-	  var clientid = 'a643943f-fd85-4801-9bd4-6c79d3e1d3c2';
-	  var token = localStorage.getItem('access_token');
-	  var jwt = sessionStorage.getItem('jwt');
-	  var targetpath = '/chocopon0899gmailcom-dev/sb/openapi/bloodinformation';
-
-	  getinfo4(clientid, token);
-	  
-
-	function getinfo4(clientid, token){
-			  
-				$http({
-			       method: method,
-			       headers : {
-			           'Content-Type' : 'application/x-www-form-urlencoded;charset=utf-8'
-			       },
-			       transformRequest: $httpParamSerializerJQLike,
-			       url: url,
-			       data: { clientid: clientid, access_token: token, jwt: jwt,targetpath: targetpath}
-			     }).then(function successCallback(response){
+		$http({
+		       method: 'POST',
+		       headers : {
+		           'Content-Type' : 'application/x-www-form-urlencoded;charset=utf-8'
+		       },
+		       transformRequest: $httpParamSerializerJQLike,
+		       url: url_apis,
+			   data: { clientid: clientid, access_token: token, targetpath: targetpath_blood}
+			   
+		        }).then(function successCallback(response){
 
 			        resdata = response.data;
 
@@ -464,6 +293,5 @@ ViewApp.controller('RadarController', ['$scope', '$http', '$location', '$httpPar
 		   	   	     $location.path('/error');
 				      
 			  })
-	  };
-	  	  
-}]);
+  	  
+  }]);
