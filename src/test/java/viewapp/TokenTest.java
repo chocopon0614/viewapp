06e0755b9e5457edc94985fcfb6c80395b9a938b
atsushimori <atsushimori@localhost>
2021-11-20 23:06:30 +0900

    unit test

--- src/test/java/viewapp/TokenTest.java
+++ src/test/java/viewapp/TokenTest.java
@@ -3,6 +3,7 @@ package viewapp;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 import static org.mockito.Mockito.doReturn;
+import static org.mockito.Mockito.doThrow;
 
 import java.net.URI;
 import java.net.URISyntaxException;
@@ -14,7 +15,6 @@ import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.Mockito;
 import org.mockito.MockitoAnnotations;
-import org.mockito.Spy;
 import org.mockito.junit.jupiter.MockitoExtension;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -29,7 +29,7 @@ public class TokenTest {
 
 	@Mock
 	private RestTemplate restTemplate;
-	
+
 	@InjectMocks
 	private Token tokenController;
 
@@ -42,24 +42,53 @@ public class TokenTest {
 	void tokenrequestTestNormal() throws URISyntaxException {
 		String res = "{\"access_token\" : \"dummyAccessToken\"}";
 		ResponseEntity<String> dummyRes = new ResponseEntity<>(res, HttpStatus.OK);
-		
-	    URI location = new URI("dummySuccessUrl" + "dummyAccessToken");
 
-		try {
-			doReturn("dummyClientId").when(prop).getClientId();
-			doReturn("dummyTokenUrl").when(prop).getTokenUrl();
-			doReturn("dummySuccessUrl").when(prop).getSuccessUrl();
-			doReturn(dummyRes).when(restTemplate).postForEntity(Mockito.anyString(), Mockito.any(), Mockito.any());
+		URI location = new URI("dummySuccessUrl" + "dummyAccessToken");
 
-			ResponseEntity<String> result = tokenController.tokenRequest("dummyCode", null);
-			assertEquals(HttpStatus.FOUND, result.getStatusCode());
-			assertTrue(result.getHeaders().containsKey("Location"));
-			assertEquals(location.toString(),
-					result.getHeaders().getFirst("Location"));
-			  
-		} catch (Exception e) {
-			e.printStackTrace();
-		}
+		doReturn("dummyTokenUrl").when(prop).getTokenUrl();
+		doReturn("dummySuccessUrl").when(prop).getSuccessUrl();
+		doReturn(dummyRes).when(restTemplate).postForEntity(Mockito.anyString(), Mockito.any(), Mockito.any());
+
+		ResponseEntity<String> result = tokenController.tokenRequest("dummyCode", null);
+		assertEquals(HttpStatus.FOUND, result.getStatusCode());
+		assertTrue(result.getHeaders().containsKey("Location"));
+		assertEquals(location.toString(), result.getHeaders().getFirst("Location"));
+
+	}
+
+	@Test
+	void tokenrequestTestError1() throws URISyntaxException {
+
+		URI location = new URI("dummyErrorUrl");
+
+		doReturn("dummyErrorUrl").when(prop).getErrorUrl();
+
+		ResponseEntity<String> result = tokenController.tokenRequest(null, "dummyError");
+		assertEquals(HttpStatus.FOUND, result.getStatusCode());
+		assertTrue(result.getHeaders().containsKey("Location"));
+		assertEquals(location.toString(), result.getHeaders().getFirst("Location"));
+
+	}
+
+	@Test
+	void tokenrequestTestError2() {
+		ResponseEntity<String> errorRes = new ResponseEntity<>(HttpStatus.FORBIDDEN);
+
+		doReturn("dummyTokenUrl").when(prop).getTokenUrl();
+		doReturn(errorRes).when(restTemplate).postForEntity(Mockito.anyString(), Mockito.any(), Mockito.any());
+
+		ResponseEntity<String> result = tokenController.tokenRequest("dummyCode", null);
+		assertEquals(HttpStatus.INTERNAL_SERVER_ERROR, result.getStatusCode());
+
+	}
+
+	@Test
+	void tokenrequestTestException() {
+		doThrow(new RuntimeException()).when(restTemplate).postForEntity(Mockito.anyString(), Mockito.any(),
+				Mockito.any());
+
+		ResponseEntity<String> result = tokenController.tokenRequest("dummyCode", null);
+		assertEquals(HttpStatus.INTERNAL_SERVER_ERROR, result.getStatusCode());
 
 	}
 
